d5615b4bd5d2b909672b0a9733c276f8800a2846
artem.savchenko <artem.savchenko@victoriaml.com>
2023-05-17 14:17:22 +0200

    Refactor docker compose and utils

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,12 +1,10 @@
-import os
 import sys
 import time
 import json
 from datetime import datetime
 
-from kafka import KafkaProducer, KafkaConsumer, errors
-
-OFFSET_FILE = "last_offset.txt"
+from kafka import KafkaProducer, KafkaConsumer
+from kafka.errors import KafkaError, NoBrokersAvailable, KafkaTimeoutError
 
 
 class DateTimeEncoder(json.JSONEncoder):
@@ -20,42 +18,50 @@ class DateTimeEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
-def create_producer(retry_attempts=5, retry_delay=10, kafka_topic_=None, kafka_bootstrap_servers_=None):
+def create_producer(retry_attempts=5, retry_delay=10, kafka_bootstrap_servers_=None):
+    producer = None
     for _ in range(retry_attempts):
         try:
             producer = KafkaProducer(
                 bootstrap_servers=kafka_bootstrap_servers_,
                 value_serializer=lambda v: json.dumps(v, cls=DateTimeEncoder).encode('utf-8')
             )
+            print(f'Kafka producer created for bootstrap servers: {kafka_bootstrap_servers_}')
             break
-        except errors.NoBrokersAvailable:
-            print("No brokers available. Retrying in {} seconds...".format(retry_delay))
+        except KafkaError as e:
+            print(f"Error creating Kafka producer: {e}. Retrying in {retry_delay} seconds...")
             time.sleep(retry_delay)
-    else:
-        print("Failed to connect to Kafka broker after {} attempts.".format(retry_attempts))
+
+    if producer is None:
+        print(f"Failed to connect to Kafka broker after {retry_attempts} attempts.")
         sys.exit(1)
-    print(f'Kafka producer created for {kafka_topic_} topic, {kafka_bootstrap_servers_} bootstrap servers')
 
     return producer
 
 
-def create_consumer(offset, retry_attempts=5, retry_delay=10, kafka_topic_=None, kafka_bootstrap_servers_=None):
+def create_consumer(offset='earliest', retry_attempts=5, retry_delay=10, kafka_topic_=None,
+                    kafka_bootstrap_servers_=None, group_id=None, max_poll_interval_ms=600000):
+    consumer = None
     for _ in range(retry_attempts):
         try:
             consumer = KafkaConsumer(
                 kafka_topic_,
-                group_id='my-group',
+                group_id=group_id,
                 auto_offset_reset=offset,
                 enable_auto_commit=True,
                 auto_commit_interval_ms=10000,
                 bootstrap_servers=kafka_bootstrap_servers_,
                 value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+                max_poll_interval_ms=max_poll_interval_ms
             )
+            print(f'Kafka consumer created for {kafka_topic_} topic, {kafka_bootstrap_servers_} bootstrap servers')
             break
-        except errors.NoBrokersAvailable:
-            print("No brokers available. Retrying in {} seconds...".format(retry_delay))
+        except KafkaError as e:
+            print(f"Error creating Kafka consumer: {e}. Retrying in {retry_delay} seconds...")
             time.sleep(retry_delay)
-    print(f'Kafka consumer created for {kafka_topic_} topic, {kafka_bootstrap_servers_} bootstrap servers')
+
+    if consumer is None:
+        raise KafkaError("Failed to create Kafka consumer after all retry attempts")
 
     return consumer
 
@@ -68,7 +74,7 @@ def get_offset_id(consumer):
             consumer.poll(timeout_ms=100, max_records=1)
             partition = consumer.assignment()
             break
-        except errors.KafkaTimeoutError:
+        except KafkaTimeoutError:
             time.sleep(10)
     partition = list(partition)[0]
     end_offset = consumer.end_offsets([partition])
